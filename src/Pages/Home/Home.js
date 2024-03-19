39e710b49a0e0bc61c3cf67b756c9d8e66dd9688
Nyakuma Wal <Nyakuma.wal@gmail.com>
2024-03-19 19:33:45 -0400

    Second commit

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,17 @@
+import Button from "../../Component/Button/Button";
+import Header from "../../Component/Header/Header";
+
+function HeaderPage() {
+  Return(
+    <div>
+      <div className="Header">
+        <Header />
+      </div>
+      <div>
+        <Button text={ClickM} />
+      </div>
+    </div>
+  );
+}
+
+export default HeaderPage;
